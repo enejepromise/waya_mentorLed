53e420aaa291dd3324c323f0cab2437f4c6fd9a9
unknown <enejepromise@gmail.com>
2025-05-27 10:20:52 -0700

    update

--- waya_backend/urls.py
+++ waya_backend/urls.py
@@ -1,16 +1,10 @@
-# waya_backend/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
-from users.views import home
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # App routes
-    path('api/users/', include('users.urls')),          # user registration, login, google login etc.
-    path('api/children/', include('children.urls')),    # child create, login, list, etc.
-    path('api/tasks/', include('taskmaster.urls')),
-    path('', home, name='home'),
+    path('users/', include('users.urls')),
+    path('children/', include('children.urls')),
+    path('taskmaster/', include('taskmaster.urls')),
+    
 ]
