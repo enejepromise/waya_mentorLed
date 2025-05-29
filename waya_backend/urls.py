186bac08189edb441ecc708ce14dd5301b7b12dd
unknown <enejepromise@gmail.com>
2025-05-28 23:40:51 -0700

    update redirect

--- waya_backend/urls.py
+++ waya_backend/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
+def redirect_root(request):  
+    return redirect('/users/')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('children/', include('children.urls')),
     path('taskmaster/', include('taskmaster.urls')),
+    path('', redirect_root),
     
 ]
