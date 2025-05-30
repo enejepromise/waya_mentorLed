48358c2a2b7061966241cb7b34ff962584ef4167
unknown <enejepromise@gmail.com>
2025-05-30 10:57:28 -0700

    update signal

--- users/signals.py
+++ users/signals.py
@@ -21,13 +21,27 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     send_email_task.delay(subject, user.email, html_content)
 
 
-def send_verification_email(user):
+def send_verification_email(user): 
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
     verify_url = f"{settings.FRONTEND_URL}/verify-email/?uidb64={uid}&token={token}"
+    
     subject = "Verify Your Waya Account Email"
-    html_content = render_to_string('emails/email_verification.html', {
-        'user': user,
-        'verify_url': verify_url,
-    })
+    
+    html_content = f"""
+    <html>
+        <body>
+            <p>Hello {user.username},</p>
+            <p>Thank you for registering with Waya!</p>
+            <p>Please verify your email address by clicking the link below:</p>
+            <p><a href="{verify_url}">Verify Email</a></p>
+            <p>If the above link doesn't work, copy and paste the following URL into your browser:</p>
+            <p>{verify_url}</p>
+            <br>
+            <p>Thanks,<br>Waya Team</p>
+        </body>
+    </html>
+    """
+
     send_email_task.delay(subject, user.email, html_content)
+
