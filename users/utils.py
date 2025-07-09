d8513650377fd0b05349e3f91299f431810fa74d
enejepromise <128715094+enejepromise@users.noreply.github.com>
2025-07-09 15:01:26 +0100

    Update utils.py

--- users/utils.py
+++ users/utils.py
@@ -1,20 +1,23 @@
 # utils.py
 import sendgrid
-from sendgrid.helpers.mail import Mail, TrackingSettings, ClickTracking
+from sendgrid.helpers.mail import Mail, Email, To, Content, TrackingSettings, ClickTracking
 from django.conf import settings
 
 def send_email(subject, message, to_email):
     sg = sendgrid.SendGridAPIClient(api_key=settings.SENDGRID_API_KEY)
-    email = Mail(
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        to_email=to_email,  
-        subject=subject,
-        plain_text_content=message,
-    )
 
+    from_email = Email(settings.DEFAULT_FROM_EMAIL)
+    to_email = To(to_email)
+    content = Content("text/plain", message)
+
+    mail = Mail(from_email=from_email, to_email=to_email, subject=subject)
+    mail.add_content(content)
+
+    # Disable click tracking (optional)
     tracking_settings = TrackingSettings()
     tracking_settings.click_tracking = ClickTracking(enable=False, enable_text=False)
-    email.tracking_settings = tracking_settings
-    response = sg.send(email)
+    mail.tracking_settings = tracking_settings
+
+    response = sg.send(mail)
     return response
 
