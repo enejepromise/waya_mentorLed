55b8a5d4551e57f7e3ebf367d4636fe28489edb6
enejepromise <128715094+enejepromise@users.noreply.github.com>
2025-07-09 15:13:25 +0100

    Update utils.py

--- users/utils.py
+++ users/utils.py
@@ -2,22 +2,46 @@
 import sendgrid
 from sendgrid.helpers.mail import Mail, Email, To, Content, TrackingSettings, ClickTracking
 from django.conf import settings
+import logging
 
-def send_email(subject, message, to_email):
-    sg = sendgrid.SendGridAPIClient(api_key=settings.SENDGRID_API_KEY)
+logger = logging.getLogger(__name__)
 
-    from_email = Email(settings.DEFAULT_FROM_EMAIL)
-    to_email = To(to_email)
-    content = Content("text/plain", message)
+def send_email(subject, plain_text_message, html_message, to_email):
+    """
+    Send an email using SendGrid with both plain text and HTML content.
 
-    mail = Mail(from_email=from_email, to_email=to_email, subject=subject)
-    mail.add_content(content)
+    Args:
+        subject (str): Email subject
+        plain_text_message (str): Plain text version of the email body
+        html_message (str): HTML version of the email body
+        to_email (str or list): Recipient email address or list of addresses
 
-    # Disable click tracking (optional)
-    tracking_settings = TrackingSettings()
-    tracking_settings.click_tracking = ClickTracking(enable=False, enable_text=False)
-    mail.tracking_settings = tracking_settings
+    Returns:
+        sendgrid.helpers.mail.Response or None: SendGrid response object or None if error occurs
+    """
+    try:
+        sg = sendgrid.SendGridAPIClient(api_key=settings.SENDGRID_API_KEY)
 
-    response = sg.send(mail)
-    return response
+        from_email = Email(settings.DEFAULT_FROM_EMAIL)
+        to_email_obj = To(to_email)  # Supports string or list of emails
+
+        mail = Mail(from_email=from_email, to_email=to_email_obj, subject=subject)
+
+        # Add both plain text and HTML content
+        mail.add_content(Content("text/plain", plain_text_message))
+        mail.add_content(Content("text/html", html_message))
+
+        # Disable click tracking (optional)
+        tracking_settings = TrackingSettings()
+        tracking_settings.click_tracking = ClickTracking(enable=False, enable_text=False)
+        mail.tracking_settings = tracking_settings
+
+        response = sg.send(mail)
+        logger.info(f"Email sent to {to_email} with status code {response.status_code}")
+        return response
+
+    except Exception as e:
+        logger.error(f"SendGrid error while sending email to {to_email}: {e}", exc_info=True)
+        return None
+
 
