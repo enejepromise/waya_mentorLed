8b5a0cb3b2af5cae66cde0da581b72de822f2270
enejepromise <128715094+enejepromise@users.noreply.github.com>
2025-07-09 14:23:46 +0100

    Update utils.py

--- users/utils.py
+++ users/utils.py
@@ -7,7 +7,7 @@ def send_email(subject, message, to_email):
     sg = sendgrid.SendGridAPIClient(api_key=settings.SENDGRID_API_KEY)
     email = Mail(
         from_email=settings.DEFAULT_FROM_EMAIL,
-        to_emails=to_email,
+        to_email=to_email,  
         subject=subject,
         plain_text_content=message,
     )
@@ -17,3 +17,4 @@ def send_email(subject, message, to_email):
     email.tracking_settings = tracking_settings
     response = sg.send(email)
     return response
+
